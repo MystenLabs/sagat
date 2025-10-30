4f30e717d72e03b8a001bdf87b3a74248ebf749e
Thoralf Müller <thoralf.mue@gmail.com>
2025-10-30 08:45:33 +0100

    update copyright headers

--- eslint.config.js
+++ eslint.config.js
@@ -74,6 +74,7 @@ export default [
 				'line',
 				[
 					' Copyright (c) Mysten Labs, Inc.',
+					' Modifications Copyright (c) 2025 IOTA Stiftung',
 					' SPDX-License-Identifier: Apache-2.0',
 				],
 				2,
