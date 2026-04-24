1d520641e90c25af814a094a731fd205ab660c87
Manos Liolios <emmanouil.liolios@gmail.com>
2026-04-24 16:04:01 +0300

    Bump deps

--- app/src/components/ui/sonner.tsx
+++ app/src/components/ui/sonner.tsx
@@ -13,14 +13,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 	return (
 		<Sonner
 			theme={theme as ToasterProps['theme']}
-			className="toaster group"
-			style={
-				{
-					'--normal-bg': 'var(--popover)',
-					'--normal-text': 'var(--popover-foreground)',
-					'--normal-border': 'var(--border)',
-				} as React.CSSProperties
-			}
+			className="toaster group [--normal-bg:var(--popover)] [--normal-text:var(--popover-foreground)] [--normal-border:var(--border)]"
 			{...props}
 		/>
 	);
